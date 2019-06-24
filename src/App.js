77faa2637a275544c95a4cb01cc36a10f22422df
congart <chinnawat.ongart@agoda.com>
2019-06-24 18:06:32 +0700

    Init project

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <BarContainer percent={50} />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +22,20 @@ function App() {
       </header>
     </div>
   );
+}
+const BarContainer = ({percent}) => {
+  const defaultHeight = 100
+  return (
+    <div style={{display:'flex', alignItems: 'flex-end'}}>
+      <Bar height={defaultHeight} backgroundColor='#faebd7'/>
+      <Bar height={defaultHeight*(1+percent*0.01)} backgroundColor='#ff6f61'/>
+    </div>
+  )
+}
+const Bar = (props) => {
+  return (
+    <div style={{background: props.backgroundColor, margin:'10px', width:'50px', height:props.height }} />
+  )
 }
 
 export default App;
